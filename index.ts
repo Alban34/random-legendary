528e42247c7973fd5210f6aff7d58aca27b18005
Alban Fayard <alban.fayard@agilent.com>
2022-10-10 11:26:20 +0200

    Introduce a class to manage player configuration

--- index.ts
+++ index.ts
@@ -1,61 +1,24 @@
 import { CardDrawer } from './card-drawer';
 import { DataManager } from './data-manager';
+import { PlayerConfig } from './player-config';
 
-const playerCount: number = 4;
-
-let villainsCount = 1;
-let henchmenCount = 1;
-let heroesCount = 3;
-let bystandersCount = 1;
-switch (playerCount) {
-    case 2: {
-        villainsCount = 2;
-        henchmenCount = 1;
-        heroesCount = 5;
-        bystandersCount = 2;
-        break;
-    }
-    case 3: {
-        villainsCount = 3;
-        henchmenCount = 1;
-        heroesCount = 5;
-        bystandersCount = 8;
-        break;
-    }
-    case 4: {
-        villainsCount = 3;
-        henchmenCount = 2;
-        heroesCount = 5;
-        bystandersCount = 8;
-        break;
-    }
-    case 5: {
-        villainsCount = 4;
-        henchmenCount = 2;
-        heroesCount = 6;
-        bystandersCount = 12;
-        break;
-    }
-    default: {
-        // Use default values, aka solo game
-        break;
-    }
-}
+const playerCount: number = 1;
 
 let game = {};
 
 const cardDrawer = new CardDrawer();
 const dataManager = new DataManager();
+const playerConfig = new PlayerConfig(playerCount);
 
 const legendaryBase = dataManager.loadData();
 
 game = {
     ...game, ...cardDrawer.drawRandomUnique(legendaryBase.masterminds, 'mastermind'),
     ...cardDrawer.drawRandomUnique(legendaryBase.schemes, 'scheme'),
-    ...cardDrawer.drawRandomMultiple(legendaryBase.villains, 'villain', villainsCount),
-    ...cardDrawer.drawRandomMultiple(legendaryBase.henchmen, 'henchman', henchmenCount),
-    ...cardDrawer.drawRandomMultiple(legendaryBase.heroes, 'hero', heroesCount),
-    ...{ 'bystanders': bystandersCount }
+    ...cardDrawer.drawRandomMultiple(legendaryBase.villains, 'villain', playerConfig.villainsCount),
+    ...cardDrawer.drawRandomMultiple(legendaryBase.henchmen, 'henchman', playerConfig.henchmenCount),
+    ...cardDrawer.drawRandomMultiple(legendaryBase.heroes, 'hero', playerConfig.heroesCount),
+    ...{ 'bystanders': playerConfig.bystandersCount }
 };
 
 console.log(game);
---
+++ player-config.ts
@@ -0,0 +1,64 @@
+export class PlayerConfig {
+
+    private readonly _villainsCount: number;
+    private readonly _henchmenCount: number;
+    private readonly _heroesCount: number;
+    private readonly _bystandersCount: number;
+
+    public get villainsCount() {
+        return this._villainsCount;
+    }
+
+    public get henchmenCount() {
+        return this._henchmenCount;
+    }
+
+    public get heroesCount() {
+        return this._heroesCount;
+    }
+
+    public get bystandersCount() {
+        return this._bystandersCount;
+    }
+
+    constructor(playerCount: number) {
+        switch (playerCount) {
+            case 2: {
+                this._villainsCount = 2;
+                this._henchmenCount = 1;
+                this._heroesCount = 5;
+                this._bystandersCount = 2;
+                break;
+            }
+            case 3: {
+                this._villainsCount = 3;
+                this._henchmenCount = 1;
+                this._heroesCount = 5;
+                this._bystandersCount = 8;
+                break;
+            }
+            case 4: {
+                this._villainsCount = 3;
+                this._henchmenCount = 2;
+                this._heroesCount = 5;
+                this._bystandersCount = 8;
+                break;
+            }
+            case 5: {
+                this._villainsCount = 4;
+                this._henchmenCount = 2;
+                this._heroesCount = 6;
+                this._bystandersCount = 12;
+                break;
+            }
+            default: {
+                this._villainsCount = 1;
+                this._henchmenCount = 1;
+                this._heroesCount = 3;
+                this._bystandersCount = 1;
+                break;
+            }
+        }
+
+    }
+}
