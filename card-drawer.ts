f65921cd1bc3d2e717d6d5961088473262f561c9
Alban Fayard <alban.fayard@agilent.com>
2022-10-10 14:06:11 +0200

    Improve card drawer code

--- card-drawer.ts
+++ card-drawer.ts
@@ -1,13 +1,24 @@
 export class CardDrawer {
 
-    public drawRandomUnique(category, nameInGame) {
-        return this.drawRandom(category, nameInGame);
+    /**
+     * Draw randomly a single card from the given card list
+     * @param cardList
+     * @param nameInGame
+     */
+    public drawRandomUnique(cardList, nameInGame) {
+        return this.drawRandom(cardList, nameInGame);
     }
 
-    public drawRandomMultiple(category, nameInGame, countToDraw) {
+    /**
+     * Draw randomly multiple cards from a give card list
+     * @param cardList
+     * @param nameInGame
+     * @param countToDraw
+     */
+    public drawRandomMultiple(cardList, nameInGame, countToDraw) {
         let choices = [];
         for (let i = 0; i < countToDraw; i++) {
-            choices.push(this.drawRandom(category, nameInGame)[nameInGame]);
+            choices.push(this.drawRandom(cardList, nameInGame)[nameInGame]);
         }
 
         let choicesForGame = {};
@@ -20,29 +31,19 @@ export class CardDrawer {
         return Math.floor(Math.random() * max);
     }
 
-    private sortLists(a, b) {
-        if (a.count === b.count) {
-            return 0;
-        }
-        if (a.count > b.count) {
-            return 1;
-        }
-        return -1;
-    }
-
     private filterList(element, countToKeep) {
         return element.count === countToKeep;
     }
 
-    private drawRandom(category, nameInGame) {
-        category = category.sort(this.sortLists);
+    private drawRandom(cardList, nameInGame) {
+        const lowestCount = cardList.reduce((prev, curr) => {
+            return prev.count < curr.count ? prev : curr;
+        }).count;
 
-        const valueToFilter = category[0].count;
-
-        const filteredMasterminds = category.filter((value) => this.filterList(value, valueToFilter));
+        const filteredCardList = cardList.filter((value) => this.filterList(value, lowestCount));
 
         let choiceForGame = {};
-        const choice = filteredMasterminds[this.getRandomInt(filteredMasterminds.length)];
+        const choice = filteredCardList[this.getRandomInt(filteredCardList.length)];
         choiceForGame[nameInGame] = choice;
 
         choice.count++;
