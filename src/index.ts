a322fa35eb31d8e9ab01b0686df3dd353c4316f4
Alban Fayard <alban.fayard@agilent.com>
2022-10-11 14:57:46 +0200

    Improving use of inquirer to be able to enter a score

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,61 @@ import { GameBuilder } from './game-builder';
 
 const inquirer = require('inquirer');
 
-const question = [
+const questions = [
+    {
+        name: 'option',
+        type: 'list',
+        choices: ['Start a new game', 'Enter a game score'],
+        default: 0
+    },
     {
         name: 'playerCount',
         type: 'number',
         default: 1,
-        message: 'How many players will participate to this game?'
+        message: 'How many players will participate to this game?',
+        when: (answer => answer.option === 'Start a new game')
+    },
+    {
+        name: 'gameId',
+        type: 'string',
+        message: 'Which is the id of the game you played?',
+        when: (answer => answer.option === 'Enter a game score')
+    },
+    {
+        name: 'score',
+        type: 'number',
+        message: 'What is your score?',
+        when: (answer => answer.gameId)
     }
+
 ];
 
-inquirer.prompt(question)
+inquirer.prompt(questions)
     .then(answer => {
-        const playerCount = answer.playerCount;
-
-        const dataManager = new DataManager();
-        const playerConfig = new PlayerConfig(playerCount);
-        const gameBuilder = new GameBuilder();
-        const legendaryBase = dataManager.loadData();
-
-        const game = gameBuilder.buildGame(legendaryBase, playerConfig);
-
-        dataManager.saveData(legendaryBase);
-
-        const gameViewer = new GameViewer();
-        console.log(gameViewer.buildView(playerCount, game));
-
+        if (answer.playerCount) {
+            startGame(answer.playerCount);
+        }
+        if (answer.score) {
+            recordScore(answer.gameId, answer.score);
+        }
     });
+
+const startGame = (playerCount: number) => {
+
+    const dataManager = new DataManager();
+    const playerConfig = new PlayerConfig(playerCount);
+    const gameBuilder = new GameBuilder();
+    const legendaryBase = dataManager.loadData();
+
+    const game = gameBuilder.buildGame(legendaryBase, playerConfig);
+
+    dataManager.saveData(legendaryBase);
+
+    const gameViewer = new GameViewer();
+    console.log(gameViewer.buildView(playerCount, game));
+};
+
+const recordScore = (gameId: string, score: number) => {
+    console.log(`Congratulation for scoring ${score} on game with id ${gameId}`);
+}
+
