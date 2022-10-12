64214cedb359c487f06449381e21313aed719b47
Alban Fayard <alban.fayard@agilent.com>
2022-10-12 13:43:22 +0200

    Improve game selection for scoring

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,28 @@ import { DataManager } from './data-manager';
 import { PlayerConfig } from './player-config';
 import { GameViewer } from './game-viewer';
 import { GameBuilder } from './game-builder';
+import { GameManager } from './game-manager';
 
 const inquirer = require('inquirer');
+
+const dataManager = new DataManager();
+const legendaryBase = dataManager.loadData();
+const gameManager = new GameManager();
+const availableGamesForScore = gameManager.loadRegisteredGame(legendaryBase);
+
+const buildInitialQuestion = () => {
+    const choices = ['Start a new game'];
+    if (availableGamesForScore.length > 0) {
+        choices.push('Enter a game score');
+    }
+    return choices;
+};
 
 const questions = [
     {
         name: 'option',
         type: 'list',
-        choices: ['Start a new game', 'Enter a game score'],
+        choices: () => buildInitialQuestion(),
         default: 0
     },
     {
@@ -21,7 +35,8 @@ const questions = [
     },
     {
         name: 'gameId',
-        type: 'string',
+        type: 'list',
+        choices: availableGamesForScore,
         message: 'Which is the id of the game you played?',
         when: (answer => answer.option === 'Enter a game score')
     },
@@ -44,13 +59,10 @@ inquirer.prompt(questions)
         }
     });
 
-const dataManager = new DataManager();
-
 const startGame = (playerCount: number) => {
 
     const playerConfig = new PlayerConfig(playerCount);
     const gameBuilder = new GameBuilder();
-    const legendaryBase = dataManager.loadData();
 
     const game = gameBuilder.buildGame(legendaryBase, playerConfig);
 
@@ -63,4 +75,4 @@ const startGame = (playerCount: number) => {
 const recordScore = (gameId: string, score: number) => {
     console.log(`Congratulation for scoring ${score} on game with id '${gameId}'.\nYour score has been saved in scores.json file.`);
     dataManager.saveScore(gameId, score);
-}
+};
