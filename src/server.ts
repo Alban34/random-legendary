3bc69715306130c59412b12ee93f8aefdf2d0cd5
Alban Fayard <alban.fayard@gmail.com>
2022-10-20 16:42:54 +0200

    Introducing multiple path + css separated file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,47 @@
 import http from 'http';
+import fs from 'fs-extra';
 import { SetupWebViewer } from './viewer/web/setup-web-viewer';
 
-http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
+http.createServer((request, response) => {
     const setupWebViewer = new SetupWebViewer();
-    response.write(setupWebViewer.showUI());
-    response.end();
+    switch (request.url) {
+        case '/':
+            setHTMLResponse(setupWebViewer.showUI(), response);
+            break;
+        case '/showExtensions':
+            setHTMLResponse(setupWebViewer.showExtensions(), response);
+            break;
+        case '/styles.css':
+            setFileResponse('./assets/styles.css', response);
+            break;
+        default:
+            response.writeHead(404);
+            response.end();
+    }
 }).listen(9615);
+
+const setHTMLResponse = (content: string, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(`
+        <html lang="">
+        <head>
+            <link rel="stylesheet" href="/styles.css">
+            <title>Legendary Marvel randomizer</title>
+        </head>
+            <body>${content}</body>
+        </html>`);
+    response.end();
+};
+
+const setFileResponse = (path: string, response) => {
+    fs.readFile(path, (err, data) => {
+        if (!err) {
+            response.setHeader('Content-type', 'text/css');
+            response.end(data);
+        } else {
+            console.log(`file not found: ${path}`);
+            response.writeHead(404, 'Not Found');
+            response.end();
+        }
+    });
+};
--- src/viewer/web/setup-web-viewer.ts
+++ src/viewer/web/setup-web-viewer.ts
@@ -1,23 +1,49 @@
 import { GameManager } from '../../game/game-manager';
 import { ALL_CARDS } from '../../card/base-cards';
+import { CardManager } from '../../card/card-manager';
+import { CardLoader } from '../../card/card-loader';
 
 export class SetupWebViewer {
 
     private choices;
 
     constructor() {
-        this.choices = ['Start a new game'];
+        this.choices = [{ label: 'Start a new game', path: '' }];
         const gameManager = new GameManager();
         const availableGamesForScore = gameManager.loadRegisteredGameWithNoScore(ALL_CARDS);
         if (availableGamesForScore.length > 0) {
-            this.choices.push('Enter a game score');
+            this.choices.push({ label: 'Enter a game score', path: '' });
         }
-        this.choices.push(...['Select your extensions', 'Show all available cards', 'Show my cards (from selected extensions)']);
+        this.choices.push(...[
+            { label: 'Select your extensions', path: '/showExtensions' },
+            { label: 'Show all available cards', path: '' },
+            { label: 'Show my cards (from selected extensions)', path: '' }]);
     }
 
     public showUI(): string {
         let webChoices: string = '';
-        this.choices.forEach(choice => webChoices += `<button>${choice}</button>`);
+        this.choices.forEach(choice => webChoices += `<a href="${choice.path}">${choice.label}</a>`);
         return webChoices;
+    }
+
+    public showExtensions(): string {
+
+        const cardLoader = new CardLoader();
+        const cardManager = new CardManager();
+        const allExtensions = cardManager.getAvailableExtensions(ALL_CARDS);
+        let selectedExtensions = cardLoader.loadExtensions();
+        if (selectedExtensions.length === 0) {
+            selectedExtensions = allExtensions;
+        }
+
+        const availableExtensionsAsWeb = allExtensions.map(ext => {
+            return `
+                <input type='checkbox' id='${ext}' checked='${selectedExtensions.indexOf(ext) > -1}'>
+                <label for='${ext}'>${ext}</label>
+            `;
+        });
+        let webContent = availableExtensionsAsWeb.join('');
+        webContent += `<button>Cancel</button><button>Save</button>`;
+        return webContent;
     }
 }
