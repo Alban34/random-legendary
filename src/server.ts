92e9ad452e9bc950b8203dde0ca5dae5ca3d8ef7
Alban Fayard <alban.fayard@gmail.com>
2022-10-25 21:24:31 +0200

    Improve server code base

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import fs from 'fs-extra';
+import express from 'express';
 import { SetupWebViewer } from './viewer/web/setup-web-viewer';
 import { CardLoader } from './card/card-loader';
 import { ScoreInputParser } from './viewer/web/score-input-parser';
 import { GameDataManager } from './game/game-data-manager';
-import express from 'express';
 
 const setupWebViewer = new SetupWebViewer();
 const app = express();
@@ -14,83 +14,54 @@ app.listen(process.env.PORT || 3000, () => {
     console.log('Starting Legendary Marvel randomizer on: http://localhost:3000');
 });
 
-app.get('/', (req, res) => {
-    setHTMLResponse(setupWebViewer.showUI(), res);
-});
-
-app.get('/styles.css', (req, res) => {
-    setFileResponse('./assets/styles.css', res);
-});
-
-app.get('/bootstrap.css', (req, res) => {
-    setFileResponse('./node_modules/bootstrap/dist/css/bootstrap.min.css', res);
-});
-
-app.get('/bootstrap.min.css.map', (req, res) => {
-    setFileResponse('./node_modules/bootstrap/dist/css/bootstrap.min.css.map', res);
-});
-
-app.get('/bootstrap.js', (req, res) => {
-    setFileResponse('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', res);
-});
-
-app.get('/bootstrap.bundle.min.js.map', (req, res) => {
-    setFileResponse('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map', res);
-});
-
+app.get('/', (req, res) => writeHTMLResponse(setupWebViewer.showUI(), res));
 app.get('/newGame', (req, res) => {
     const playerCount = parseInt(req.url.substring(req.url.lastIndexOf('=') + 1));
-    setHTMLResponse(setupWebViewer.startGame(playerCount), res);
+    writeHTMLResponse(setupWebViewer.startGame(playerCount), res);
 });
+app.get('/enterScore', (req, res) => writeHTMLResponse(setupWebViewer.showAvailableGameForScore(), res));
+app.get('/showAllCards', (req, res) => writeHTMLResponse(setupWebViewer.showCards(), res));
+app.get('/showMyCards', (req, res) => writeHTMLResponse(setupWebViewer.showCards(false), res));
+app.get('/showExtensions', (req, res) => writeHTMLResponse(setupWebViewer.showExtensions(), res));
 
-app.get('/enterScore', (req, res) => {
-    setHTMLResponse(setupWebViewer.showAvailableGameForScore(), res);
-});
+app.post('/saveExtensions', (req, res) => handlePost(req, res, saveExtensions));
+app.post('/saveScores', (req, res) => handlePost(req, res, saveScores));
 
-app.get('/showAllCards', (req, res) => {
-    setHTMLResponse(setupWebViewer.showCards(), res);
-});
+app.get('/styles.css', (req, res) => writeFileResponse('./assets/styles.css', res));
+app.get('/bootstrap.css', (req, res) => writeFileResponse('./node_modules/bootstrap/dist/css/bootstrap.min.css', res));
+app.get('/bootstrap.min.css.map', (req, res) => writeFileResponse('./node_modules/bootstrap/dist/css/bootstrap.min.css.map', res));
+app.get('/bootstrap.js', (req, res) => writeFileResponse('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', res));
+app.get('/bootstrap.bundle.min.js.map', (req, res) => writeFileResponse('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map', res));
 
-app.get('/showMyCards', (req, res) => {
-    setHTMLResponse(setupWebViewer.showCards(false), res);
-});
-
-app.get('/showExtensions', (req, res) => {
-    setHTMLResponse(setupWebViewer.showExtensions(), res);
-});
-
-let chunks = [];
-app.post('/saveExtensions', (req, res) => {
+const handlePost = (req, res, callback) => {
+    const chunks = [];
     req.on('data', chunk => chunks.push(chunk));
-    req.on('end', () => {
-        const data = Buffer.concat(chunks).toString()
-            .replace(/&/g, '')
-            .replace(/(%2C)/g, ',')
-            .replace(/\+/g, ' ');
-        const idsToSave = data.split('ext=')
-            .filter(value => value !== '');
-        const cardLoader = new CardLoader();
-        cardLoader.saveExtensions(idsToSave);
-    });
+    req.on('end', () => callback(chunks));
     res.redirect('/');
-});
+};
 
-app.post('/saveScores', (req, res) => {
-    req.on('data', chunk => chunks.push(chunk));
-    req.on('end', () => {
-        const data = Buffer.concat(chunks).toString();
-        const parser = new ScoreInputParser();
-        const scores = parser.parseInput(data);
-        const gameDataManager = new GameDataManager();
-        for (const gameId in scores) {
-            gameDataManager.saveScore(gameId, scores[gameId].score);
-        }
-    });
-    res.redirect('/');
-});
+const saveExtensions = (chunks) => {
+    const data = Buffer.concat(chunks).toString()
+        .replace(/&/g, '')
+        .replace(/(%2C)/g, ',')
+        .replace(/\+/g, ' ');
+    const idsToSave = data.split('ext=')
+        .filter(value => value !== '');
+    const cardLoader = new CardLoader();
+    cardLoader.saveExtensions(idsToSave);
+};
 
+const saveScores = (chunks) => {
+    const data = Buffer.concat(chunks).toString();
+    const parser = new ScoreInputParser();
+    const scores = parser.parseInput(data);
+    const gameDataManager = new GameDataManager();
+    for (const gameId in scores) {
+        gameDataManager.saveScore(gameId, scores[gameId].score);
+    }
+};
 
-const setHTMLResponse = (content: string, response) => {
+const writeHTMLResponse = (content: string, response) => {
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(`
         <html lang="">
@@ -108,7 +79,7 @@ const setHTMLResponse = (content: string, response) => {
     response.end();
 };
 
-const setFileResponse = (path: string, response) => {
+const writeFileResponse = (path: string, response) => {
     fs.readFile(path, (err, data) => {
         if (!err) {
             response.setHeader('Content-type', 'text/css');
