2b84dcb4e7680689ca0e47346ccb12576c4ce73a
Alban Fayard <alban.fayard@gmail.com>
2022-11-02 16:33:44 +0100

    Fix script

--- scripts/copy-bootstrap.ts
+++ scripts/copy-bootstrap.ts
@@ -1,10 +1,13 @@
 import fs from 'fs-extra';
 
+const assetGeneratedPath = './assets/generated/';
+fs.mkdirsSync(assetGeneratedPath);
+
 fs.copyFileSync('./node_modules/bootstrap/dist/css/bootstrap.min.css',
-    './assets/generated/bootstrap.min.css');
+    `${assetGeneratedPath}bootstrap.min.css`);
 fs.copyFileSync('./node_modules/bootstrap/dist/css/bootstrap.min.css.map',
-    './assets/generated/bootstrap.min.css.map');
+    `${assetGeneratedPath}bootstrap.min.css.map`);
 fs.copyFileSync('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-    './assets/generated/bootstrap.bundle.min.js');
+    `${assetGeneratedPath}bootstrap.bundle.min.js`);
 fs.copyFileSync('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map',
-    './assets/generated/bootstrap.bundle.min.js.map');
+    `${assetGeneratedPath}bootstrap.bundle.min.js.map`);
