d17baf98b39420374c1e7d4441ed8669a99c298a
Alban Fayard <alban.fayard@gmail.com>
2023-03-06 08:50:23 +0100

    Add a migration script to change the game ids

--- src/game/game-manager.ts
+++ src/game/game-manager.ts
@@ -1,12 +1,20 @@
-import { Game, GameDataManager } from './game.module';
+import { Game, GameDataManager, PlayerConfig, Scores } from './game.module';
 import { Card } from '../card/model/card';
 import { inject, injectable } from 'inversify';
 import TYPES from '../types';
+import { CardLoader } from '../card/card-loader';
+import { DataManager } from '../data/data-manager.interface';
+import { Score } from './model/scores';
+import { all } from 'inversify-express-utils';
 
 @injectable()
 export class GameManager {
 
-    constructor(@inject(TYPES.GameDataManager) private readonly gameDataManager: GameDataManager) {
+    constructor(@inject(TYPES.GameDataManager) private readonly gameDataManager: GameDataManager,
+                @inject(TYPES.CardLoader) private readonly cardLoader: CardLoader,
+                @inject(TYPES.DataManager) private readonly dataManager: DataManager) {
+        // Migrate previous gameIds to the new 4.0 version way
+        this.migrateGameIdTo4();
     }
 
     public loadRegisteredGame(cardList): string[] {
@@ -21,7 +29,6 @@ export class GameManager {
     }
 
     public getCardsOfGame(cardList, gameId: string): Game {
-
         const mastermind = this.getCardsOfGivenCategory(cardList.masterminds, gameId)[0];
         const scheme = this.getCardsOfGivenCategory(cardList.schemes, gameId)[0];
         const villains = this.getCardsOfGivenCategory(cardList.villains, gameId);
@@ -45,5 +52,41 @@ export class GameManager {
             }
             return false;
         });
+    }
+
+    private migrateGameIdTo4() {
+        const allCardList = this.cardLoader.loadData();
+        const gameIds = this.loadRegisteredGame(allCardList);
+        const gameToMigrate = gameIds.filter(gameId => gameId.indexOf('|') === -1);
+
+        if (gameToMigrate.length > 0) {
+            const allScores = this.gameDataManager.loadScores();
+            const gamesToSave = [];
+            gameToMigrate.forEach(gameId => {
+                const game = this.getCardsOfGame(allCardList, gameId);
+                const guessedPlayerCount = PlayerConfig.guessPlayerCount(game.villains.length, game.henchmen.length, game.heroes.length);
+                const newId = `${gameId}|${guessedPlayerCount}`;
+
+                this.migrateGameIdInCard(game.mastermind, gameId, newId);
+                this.migrateGameIdInCard(game.scheme, gameId, newId);
+                game.villains.forEach(c => this.migrateGameIdInCard(c, gameId, newId));
+                game.henchmen.forEach(c => this.migrateGameIdInCard(c, gameId, newId));
+                game.heroes.forEach(c => this.migrateGameIdInCard(c, gameId, newId));
+
+                gamesToSave.push(game);
+                allScores[newId] = allScores[gameId];
+                delete allScores[gameId];
+            });
+            this.dataManager.writeGameData(gamesToSave);
+            this.dataManager.writeScores(allScores);
+            console.log(`${gameToMigrate.length} games migrated to the new 4.0 version way.`);
+        }
+    }
+
+    private migrateGameIdInCard(card: Card, gameId: string, newId: string) {
+        const index = card.gameId.indexOf(gameId);
+        if (index !== -1) {
+            card.gameId[index] = newId;
+        }
     }
 }
